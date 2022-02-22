074d2e300474c67e40d2b57eb0851eda7d7d6e4b
jeong9216 <dbwjdwn9216@gmail.com>
2022-02-22 15:39:38 +0900

    Update: 프로그래머스 키패드 누르기

--- SwiftAlgorithm/Programmers/키패드_누르기.swift
+++ SwiftAlgorithm/Programmers/키패드_누르기.swift
@@ -8,65 +8,69 @@
 import Foundation
 
 func solution(_ numbers:[Int], _ hand:String) -> String {
+    func setupCursor(_ number: Int) -> (Int, Int) {
+        var cursor = (0,0)
+        switch number {
+        case 0: cursor = (1,0)
+        case 1: cursor = (0,3)
+        case 2: cursor = (1,3)
+        case 3: cursor = (2,3)
+        case 4: cursor = (0,2)
+        case 5: cursor = (1,2)
+        case 6: cursor = (2,2)
+        case 7: cursor = (0,1)
+        case 8: cursor = (1,1)
+        case 9: cursor = (2,1)
+        default: break
+        }
+        
+        return cursor
+    }
+    
+    func addString(number: Int, leftDistance: Int, rightDistance: Int) -> String {
+        var string = ""
+        switch number {
+        case 1, 4, 7:
+            string = "L"
+        case 3, 6, 9:
+            string = "R"
+        case 0, 2, 5, 8:
+            if leftDistance < rightDistance {
+                string = "L"
+            } else if leftDistance > rightDistance {
+                string = "R"
+            } else {
+                if hand == "right" {
+                    string = "R"
+                } else {
+                    string = "L"
+                }
+            }
+        default:
+            break
+        }
+        
+        return string
+    }
+    
     var result: String = ""
         
-    let isRight = (hand == "right")
-    
     var leftCursor: (Int, Int) = (0,0)
     var rightCursor: (Int, Int) = (2,0)
     
     for number in numbers {
-        switch number {
-        case 1, 4, 7:
-            result += "L"
-            if number == 1 {
-                leftCursor = (0,3)
-            } else if number == 4 {
-                leftCursor = (0,2)
-            } else if number == 7 {
-                leftCursor = (0,1)
-            }
-        case 3, 6, 9:
-            result += "R"
-            if number == 3 {
-                rightCursor = (2,3)
-            } else if number == 6 {
-                rightCursor = (2,2)
-            } else if number == 9 {
-                rightCursor = (2,1)
-            }
-        case 0, 2, 5, 8:
-            var cursor: (Int, Int) = (1,0)
-            if number == 2 {
-                cursor = (1,3)
-            } else if number == 5 {
-                cursor = (1,2)
-            } else if number == 8 {
-                cursor = (1,1)
-            }
-            
-            let leftDistance = abs(leftCursor.0 - cursor.0) + abs(leftCursor.1 - cursor.1)
-            let rightDistance = abs(rightCursor.0 - cursor.0) + abs(rightCursor.1 - cursor.1)
-            
-            if leftDistance < rightDistance {
-                result += "L"
-                leftCursor = cursor
-            } else if leftDistance > rightDistance {
-                result += "R"
-                rightCursor = cursor
-            } else {
-                if isRight {
-                    result += "R"
-                    rightCursor = cursor
-                } else {
-                    result += "L"
-                    leftCursor = cursor
-                }
-            }
-            
-            
-        default:
-            break
+        let cursor: (Int, Int) = setupCursor(number)
+        
+        let leftDistance = abs(leftCursor.0 - cursor.0) + abs(leftCursor.1 - cursor.1)
+        let rightDistance = abs(rightCursor.0 - cursor.0) + abs(rightCursor.1 - cursor.1)
+        
+        let string: String = addString(number: number, leftDistance: leftDistance, rightDistance: rightDistance)
+        
+        result += string
+        if string == "R" {
+            rightCursor = cursor
+        } else {
+            leftCursor = cursor
         }
     }
 
