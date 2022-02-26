e2d8de0e1f84859ab03739addec66bcdd6b842d0
jeongju9216 <dbwjdwn9216@gmail.com>
2022-02-26 18:34:00 +0900

    Add: 프로그래머스 - 서울에서 김서방 찾기

---
+++ SwiftAlgorithm/Programmers/Level1/서울에서_김서방_찾기.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  SwiftAlgorithm
+//
+//  Created by 유정주 on 2022/02/26.
+//
+
+func solution(_ seoul:[String]) -> String {
+    return "김서방은 \(seoul.firstIndex(of: "Kim")!)에 있다"
+}
