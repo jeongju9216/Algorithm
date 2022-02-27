69567e1ffd11811e235abd11f4a83c65c4b9c11c
jeongju9216 <dbwjdwn9216@gmail.com>
2022-02-27 09:36:28 +0900

    Update: 프로그래머스 - 자연수 뒤집어 배열로 만들기

--- SwiftAlgorithm/Programmers/Level1/자연수_뒤집어_배열로_만들기.swift
+++ SwiftAlgorithm/Programmers/Level1/자연수_뒤집어_배열로_만들기.swift
@@ -8,5 +8,6 @@
 import Foundation
 
 func solution(_ n:Int64) -> [Int] {
+//    return String(n).reversed().map { Int(String($0))! }
     return String(n).reversed().compactMap { Int(String($0)) }
 }
