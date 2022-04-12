d04c32752236ec200280db64d81917a4e0bc1cb3
유정주 <dbwjdwn9216@gmail.com>
2022-04-13 00:10:28 +0900

    Add: 프로그래머스 - 보석 쇼핑

---
+++ SwiftAlgorithm/Programmers/Level2/보석_쇼핑.swift
@@ -0,0 +1,48 @@
+//
+//  보석_쇼핑.swift
+//  SwiftAlgorithm
+//
+//  Created by 유정주 on 2022/04/13.
+//
+
+import Foundation
+
+func solution(_ gems:[String]) -> [Int] {
+    let gemCount: Int = Set(gems).count
+    if gemCount == 1 {
+        return [1, 1]
+    }
+    
+    var gemDict: [String: Int] = [:]
+    var left = 0, right = 0, minDist = Int.max
+    var result: [Int] = [0, 0]
+    
+    while left < gems.count && right < gems.count {
+        guard let _ = gemDict[gems[left]] else {
+            gemDict[gems[left]] = 0
+            continue
+        }
+        
+        if gemDict.count == gemCount {
+            if right - left < minDist {
+                minDist = right - left
+                result = [left+1, right+1]
+            }
+
+            gemDict[gems[left]]! -= 1
+            if gemDict[gems[left]] == 0 {
+                gemDict[gems[left]] = nil
+            }
+            left += 1
+        } else {
+            if let _ = gemDict[gems[right]] {
+                gemDict[gems[right]]! += 1
+                right += 1
+            } else {
+                gemDict[gems[right]] = 0
+            }
+        }
+    }
+    
+    return result
+}
