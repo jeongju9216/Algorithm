96c1ffc678af2a55499d1c8da76f113f102e10c4
유정주 <dbwjdwn9216@gmail.com>
2022-04-18 21:57:30 +0900

    Add: 프로그래머스 - [1차] 셔틀버스

--- SwiftAlgorithm/Programmers/Level3/[1차]_셔틀버스.swift
+++ SwiftAlgorithm/Programmers/Level3/[1차]_셔틀버스.swift
@@ -7,24 +7,14 @@
 
 import Foundation
 
-func createTimeString(_ hour: Int, _ min: Int) -> String {
-    return String(format: "%02d:%02d", hour, min)
-}
-
 func createTimeString(_ min: Int) -> String {
     return String(format: "%02d:%02d", min/60, min%60)
 }
 
 func solution(_ n:Int, _ t:Int, _ m:Int, _ timetable:[String]) -> String {
-    if timetable.count+1 <= m {
-        let pTime = (n-1) * t
-        return createTimeString(540 + pTime)
-    }
-    
     //String -> Int 분으로 변환
-    var timetable: [Int] = timetable.map { t in
-        let split = t.split { $0 == ":" }.map { Int(String($0))! }
-        return split[0] * 60 + split[1]
+    var timetable: [Int] = timetable.map {
+        Int($0.prefix(2))! * 60 + Int($0.suffix(2))!
     }.sorted()
     
     var table: [Int] = []
@@ -32,6 +22,8 @@ func solution(_ n:Int, _ t:Int, _ m:Int, _ timetable:[String]) -> String {
     for _ in 0..<n { //버스 운영 횟수
         table = []
         for time in timetable {
+            //탑승 가능 인원 만큼 태웠다면 break
+            //다음 회차 시간이라면 break
             guard table.count < m, time <= sumOfMin else {
                 break
             }
@@ -44,7 +36,6 @@ func solution(_ n:Int, _ t:Int, _ m:Int, _ timetable:[String]) -> String {
     sumOfMin -= t
 
     if let max = table.max(), table.count == m {
-        print("max: \(max)")
         return createTimeString(max - 1)
     } else {
         return createTimeString(sumOfMin)
