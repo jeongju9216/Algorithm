b047472bd879041b8c443cdd84cabacd0878abb5
유정주 <dbwjdwn9216@gmail.com>
2022-05-06 10:52:52 +0900

    Update: 거리두기 확인하기 코드 수정(4월 25일 테케 추가)

--- SwiftAlgorithm/Programmers/Level2/거리두기_확인하기.swift
+++ SwiftAlgorithm/Programmers/Level2/거리두기_확인하기.swift
@@ -14,79 +14,92 @@
 
 import Foundation
 
-let dx: [Int] = [-1, 1, 0, 0]
-let dy: [Int] = [0, 0, -1, 1]
+let person = "P"
+let partition = "X"
+let size = 5
 
-func isKeepDistance(x1: Int, y1: Int, x2: Int, y2: Int, place: [[String]]) -> Bool {
-    if abs(x1 - x2) + abs(y1 - y2) > 2 {
-        return true
-    } else {
-        if x1 == x2 { //가로 사이에 파티션이 있어야 함
-            return place[x1][(y1+y2)/2] == "X"
-        } else if y1 == y2 { //세로 사이에 파티션이 있어야 함
-            return place[(x1+x2)/2][y1] == "X"
-        } else { //양쪽 다 있어야 함
-            return place[x1][(y1+y2)/2] == "X" && place[(x1+x2)/2][y1] == "X"
-        }
+func splitClassroom(_ places: [String]) -> [[String]] {
+    return places.map { $0.map { String($0) } }
+}
+
+func checkPartition(_ classroom: [[String]], a: (Int, Int), b: (Int, Int)) -> Bool {
+    if a.0 == b.0 { //같은 행이라면 세로 사이에 파티션이 있어야 한다.
+        return (classroom[a.0][(a.1+b.1)/2] == partition)
+    } else if a.1 == b.1 { //같은 열이라면 가로 사이에 파티션이 있어야 한다.
+        return (classroom[(a.0+b.0)/2][a.1] == partition)
+    } else { //대각선이라면 위아래로 파티션이 모두 있어야 한다.
+        return classroom[a.0][(a.1+b.1)/2] == partition && classroom[(a.0+b.0)/2][a.1] == partition
     }
 }
 
-func solution(_ places:[[String]]) -> [Int] {
-    var results: [Int] = []
+func checkDistance(_ classroom: [[String]], start: (Int, Int)) -> Bool {
+    let dx: [Int] = [-1, 1, 0, 0]
+    let dy: [Int] = [0, 0, -1, 1]
+
+    var queue: [(Int, Int)] = [start]
+    var index = 0
+    var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: size), count: size)
+    visited[start.0][start.1] = true
     
-    for time in 0..<5 { //강의실 5개
-        var cpPlaces: [[String]] = []
-        var people: [(x: Int, y: Int)] = []
-        for i in 0..<5 { //강의실 1개 구하기
-            cpPlaces.append([])
-            cpPlaces[i] = places[time][i].map { String($0) }
+    while index < queue.count {
+        let node = queue[index]
+        index += 1
+        
+        for i in 0..<4 {
+            let nx = node.0 + dx[i]
+            let ny = node.1 + dy[i]
+                
+            guard (0..<size) ~= nx, (0..<size) ~= ny else { continue }
             
-            for j in 0..<5 { //강의실의 사람들 좌표 구하기
-                if cpPlaces[i][j] == "P" {
-                    people.append((i, j))
+            let distance = abs(nx - start.0) + abs(ny - start.1)
+            guard distance <= 2 else { continue }
+            
+            guard !visited[nx][ny] else { continue }
+            visited[nx][ny] = true
+                        
+            if classroom[nx][ny] == person {
+                if distance <= 1 {
+                    return false
+                } else if distance == 2 { //맨해튼 거리가 2 이상이면 OK
+                    //맨해튼 거리가 2 이하일 때 파티션을 체크한다.
+                    if !checkPartition(classroom, a: start, b: (nx, ny)) { //파티션이 없으면 false
+                        return false
+                    }
                 }
+            } else if classroom[nx][ny] == "O" {
+                queue.append((nx, ny))
             }
         }
+    }
+    
+    return true
+}
+
+func solution(_ places:[[String]]) -> [Int] {
+    var result: [Int] = []
+
+    for place in places {
+        //강의실 하나를 의미하는 2차원 배열을 만든다
+        let classroom = splitClassroom(place)
+        var resultInt = 1
         
-        //사람이 없으면 거리두기 OK
-        if people.isEmpty {
-            results.append(1)
-            continue
-        }
-        
-        var result: Bool = true
-        //dfs로 탐색. 모든 사람을 각각 기준 삼아 거리 구하기
-        for pCount in 0..<people.count {
-            if !result { //거리두기를 안 했으면 더이상 탐색 X
-                break
-            }
+        for i in 0..<size {
+            guard resultInt == 1 else { break }
             
-            var stack: [(x: Int, y: Int)] = [people[pCount]]
-            var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: 5), count: 5)
-            while !stack.isEmpty && result {
-                let top = stack.popLast()!
-                visited[top.x][top.y] = true
-
-                for i in 0..<4 {
-                    let nx = top.x + dx[i]
-                    let ny = top.y + dy[i]
-
-                    if 0..<5 ~= nx && 0..<5 ~= ny { //범위 체크
-                        if !visited[nx][ny] {
-                            if cpPlaces[nx][ny] == "O" {
-                                stack.append((nx, ny))
-                            } else if cpPlaces[nx][ny] == "P" {
-                                result = isKeepDistance(x1: people[pCount].x, y1: people[pCount].y, x2: nx, y2: ny, place: cpPlaces)
-                            }
-                        }
+            for j in 0..<size {
+                //하나하나 탐색하면서 응시자를 만나면 다른 응시자와 맨해튼 거리를 측정한다.
+                if classroom[i][j] == person {
+                    //bfs를 이용해 거리 체크
+                    if !checkDistance(classroom, start: (i, j)) {
+                        resultInt = 0
+                        break
                     }
                 }
             }
         }
         
-        results.append(result ? 1 : 0)
+        result.append(resultInt)
     }
     
-    return results
+    return result
 }
-
