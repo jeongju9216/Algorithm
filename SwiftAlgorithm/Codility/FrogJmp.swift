0dfb47d73606f8b5a8c768109c8333e9f13314a9
유정주 <dbwjdwn9216@gmail.com>
2022-06-17 22:32:52 +0900

    Add: Codility - FrogJmp

--- SwiftAlgorithm/Codility/FrogJmp.swift
+++ SwiftAlgorithm/Codility/FrogJmp.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+public func solution(_ X : Int, _ Y : Int, _ D : Int) -> Int {
+    // write your code in Swift 4.2.1 (Linux)
+    return Int(ceil((Double(Y) - Double(X)) / Double(D)))
+}
