c3f21ee27e0bc6b47d9cb3b5aedb79b97c6b7c31
유정주 <dbwjdwn9216@gmail.com>
2022-06-19 09:02:52 +0900

    Add: Codility - Distinct

--- SwiftAlgorithm/Codility/Distinct.swift
+++ SwiftAlgorithm/Codility/Distinct.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+// you can write to stdout for debugging purposes, e.g.
+// print("this is a debug message")
+
+public func solution(_ A : inout [Int]) -> Int {
+    // write your code in Swift 4.2.1 (Linux)
+    return Set(A).count
+}
