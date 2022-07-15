574c4a8a003e2e9349c33c63405ef93718c78f2d
유정주 <dbwjdwn9216@gmail.com>
2022-07-15 16:20:40 +0900

    Add: 클래스 vs 구조체

--- SwiftAlgorithm/main.swift
+++ SwiftAlgorithm/main.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct PersonS {
+    var name: String = "시리"
+}
+
+class PersonC {
+    var name: String = "시리"
+    var person: PersonS = PersonS()
+}
+
+let person1: PersonC = PersonC()
+var person2 = person1
+var person3 = person1
+
+person3.person.name = "팀쿡"
+
+print("person1 name: \(person1.person.name)")
+print("person2 name: \(person2.person.name)")
+print("person3 name: \(person3.person.name)")
