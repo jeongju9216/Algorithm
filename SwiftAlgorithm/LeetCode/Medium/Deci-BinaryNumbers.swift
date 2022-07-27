8a258f2fd244261d5de661441fa5f6db4f0a701c
유정주 <dbwjdwn9216@gmail.com>
2022-07-27 12:27:07 +0900

    Add: LeetCode - 1689

---
+++ SwiftAlgorithm/LeetCode/Medium/Deci-BinaryNumbers.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  SwiftAlgorithm
+//
+//  Created by 유정주 on 2022/07/27.
+//
+
+import Foundation
+
+class Solution {
+    func minPartitions(_ n: String) -> Int {
+        return Int(String(n.max()!))!
+    }
+}
+// Runtime: 42 ms, faster than 96.61% of Swift online submissions for Partitioning Into Minimum Number Of Deci-Binary Numbers.
+// Memory Usage: 14.3 MB, less than 90.68% of Swift online submissions for Partitioning Into Minimum Number Of Deci-Binary Numbers.
