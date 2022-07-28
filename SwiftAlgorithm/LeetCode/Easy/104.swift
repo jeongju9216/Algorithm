c867ecb2da39e74fe7d751c99d62972d47a622ee
유정주 <dbwjdwn9216@gmail.com>
2022-07-28 16:54:12 +0900

    Add: Medium

--- SwiftAlgorithm/LeetCode/Easy/104.swift
+++ SwiftAlgorithm/LeetCode/Easy/104.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    var maxDepth: Int = 0
+ 
+    func maxDepth(_ root: TreeNode?) -> Int {
+        dfs(root, depth: 1)
+        return maxDepth
+    }
+    
+    func dfs(_ node: TreeNode?, depth: Int) {
+        guard let node = node else { return }
+        
+        if node.left == nil && node.right == nil {
+            maxDepth = max(maxDepth, depth)
+            return
+        }
+        
+        dfs(node.left, depth: depth + 1)
+        dfs(node.right, depth: depth + 1)
+    }
+}
--- SwiftAlgorithm/LeetCode/Medium/46.swift
+++ SwiftAlgorithm/LeetCode/Medium/46.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+class Solution {
+    
+    var result: [[Int]] = []
+    
+    func backtracking(_ list: inout [Int], _ nums: [Int]) {
+        if list.count == nums.count {
+            result.append(list)
+            return
+        }
+        
+        for num in nums {
+            if list.contains(num) {
+                continue
+            }
+            
+            list.append(num)
+            backtracking(&list, nums)
+            list.removeLast()
+        }
+    }
+    
+    func permute(_ nums: [Int]) -> [[Int]] {
+        var list: [Int] = []
+        backtracking(&list, nums)
+        
+        return result
+    }
+}
