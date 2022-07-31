859a2d604e9ff07d5e932ba29b63addcacd60883
유정주 <dbwjdwn9216@gmail.com>
2022-07-31 21:13:57 +0900

    Add: Medium

--- SwiftAlgorithm/LeetCode/Medium/39.swift
+++ SwiftAlgorithm/LeetCode/Medium/39.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+class Solution {
+    var result: [[Int]] = []
+    var candidates: [Int] = []
+    var target: Int = 0
+    
+    func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        self.candidates = candidates
+        self.target = target
+        
+        backtracking([], 0, 0)
+        
+        return result
+    }
+    
+    func backtracking(_ arr: [Int], _ index: Int, _ total: Int) {
+        if total == target {
+            result.append(arr)
+            return
+        }
+        
+        guard total < target, index < candidates.count else { return }
+        
+        let currentNum = candidates[index]
+        
+        backtracking(arr + [currentNum], index, total + currentNum)
+        backtracking(arr, index+1, total)
+    }
+}
