0725c714e398fb0491b0ac9131bc7db63385be83
유정주 <dbwjdwn9216@gmail.com>
2022-12-06 22:16:14 +0900

    Add: 연속 부분 수열 합의 개수

---
+++ SwiftAlgorithm/Programmers/Level2/연속 부분 수열 합의 개수.swift
@@ -0,0 +1,29 @@
+//
+//  연속 부분 수열 합의 개수.swift
+//  SwiftAlgorithm
+//
+//  Created by 유정주 on 2022/12/06.
+//
+
+import Foundation
+
+func solution(_ elements:[Int]) -> Int {
+    var result: Set<Int> = []
+    
+    for size in 1...elements.count { //1개 ~ n개
+        for i in 0..<elements.count {
+            var sum: Int = 0
+            for j in i..<(i+size) {
+                var index: Int = j
+                if index >= elements.count {
+                    index -= elements.count
+                }
+                
+                sum += elements[index]
+            }
+            result.insert(sum)
+        }
+    }
+    
+    return result.count
+}
