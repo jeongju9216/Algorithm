3fb174468c71b956856d0593f13b36ba9f829d26
유정주 <dbwjdwn9216@gmail.com>
2023-03-20 10:57:00 +0900

    Add: 9012

--- SwiftAlgorithm/BOJ/9012.swift
+++ SwiftAlgorithm/BOJ/9012.swift
@@ -5,29 +5,34 @@
 //  Created by 유정주 on 2022/03/03.
 //
 
-//9012 괄호
 import Foundation
 
-let count = Int(readLine()!)!
-var stack: [Character] = []
-
-for _ in 0..<count {
-    stack = []
-    
-    let input = String(readLine()!)
-    for s in input {
-        if s == "(" {
-            stack.append(s)
-        } else if s == ")" {
-            guard let pop = stack.last, pop == "(" else {
-                stack.append(s)
-                break
+func isVPS(_ string: [String]) -> Bool {
+    var stack: [String] = []
+    for str in string {
+        if str == "(" {
+            stack.append(str)
+        } else {
+            guard let last = stack.popLast() else {
+                return false
             }
             
-            stack.removeLast()
+            if last != "(" {
+                return false
+            }
         }
     }
-
-    print(stack.isEmpty ? "YES" : "NO")
+    
+    return stack.isEmpty
 }
 
+var count = Int(readLine()!)!
+for _ in 0..<count {
+    let input = Array(readLine()!).map { String($0) }
+    
+    if isVPS(input) {
+        print("YES")
+    } else {
+        print("NO")
+    }
+}
