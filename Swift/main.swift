a1e2592135d6cc6314e8cad1eb43c19c7c0abc7f
유정주 <dbwjdwn9216@gmail.com>
2024-04-24 21:14:45 +0900

    코테 준비
    프로그래머스 - 카드 뭉치

--- Swift/main.swift
+++ Swift/main.swift
@@ -1,2 +1,26 @@
 import Foundation
 
+func solution(_ cards1: [String], _ cards2: [String], _ goal: [String]) -> String {
+    var cards1: [String] = cards1.reversed()
+    var cards2: [String] = cards2.reversed()
+    
+    var result = "Yes"
+    for word in goal {
+        if let card1 = cards1.last,
+            card1 == word {
+            cards1.removeLast()
+            continue
+        }
+        
+        if let card2 = cards2.last,
+            card2 == word {
+            cards2.removeLast()
+            continue
+        }
+        
+        result = "No"
+        break
+    }
+    
+    return result
+}
